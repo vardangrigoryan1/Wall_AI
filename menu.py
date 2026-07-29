7749a3779a1135ee06b705d485e3acd049397d4e
vardangrigoryan1 <111245763+vardangrigoryan1@users.noreply.github.com>
2026-07-29 16:11:29 +0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 from ursina import *
 import environmental_mechanics
+import clustering
 
 menu_root = None
 
@@ -21,6 +22,23 @@ def choose(algo):
     destroy(menu_root)
 
 
+def run_clustering_button():
+    environmental_mechanics.set_algorithm("astar")
+    environmental_mechanics.game_started = True
+    environmental_mechanics.player.enabled = True
+
+    mouse.locked = True
+    mouse.visible = False
+
+    destroy(menu_root)
+
+    from environmental_mechanics import grid, walle, debris_positions, hazards_positions
+    agent_position = environmental_mechanics.cell(walle.position)
+    clustering.run_clustering(grid, agent_position, debris_positions, hazards_positions=hazards_positions)
+
+
+
+
 def create_menu():
     global menu_root
     menu_root = Entity(parent=camera.ui)
@@ -29,30 +47,39 @@ def create_menu():
     mouse.visible = True
 
     Text(parent=menu_root,
-         text="Choose Pathfinding Algorithm", origin=(0,0), y=0.35, scale=2)
+         text="Choose Pathfinding Algorithm :))",
+         origin=(0,0),
+         y=0.35,
+         scale=2)
     
     Button(parent=menu_root,
            text="A*",
            scale=(0.3,0.1),
-           y=0.15,
+           y=0.2,
            on_click=Func(choose, "astar"))
     
     Button(parent=menu_root,
            text="BFS",
            scale=(0.3,0.1),
-           y=0,
+           y=0.05,
            on_click=Func(choose, "bfs"))
     
     Button(parent=menu_root,
            text="DFS",
            scale=(0.3,0.1),
-           y=-0.15,
+           y=-0.1,
            on_click=Func(choose, "dfs"))
 
     Button(parent=menu_root,
            text="Greedy",
            scale=(0.3,0.1),
-           y=-0.3, 
+           y=-0.25, 
            on_click=Func(choose, "greedy"))
+
+    Button(parent=menu_root,
+           text="Clustering",
+           scale=(0.3,0.1),
+           y=-0.40,
+           on_click=Func(run_clustering_button))
 
 #©Vardan Grigoryan
