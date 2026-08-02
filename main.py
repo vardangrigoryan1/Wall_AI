190f1b9d15f3a84e21aa313eb6bff1dbc3208c87
vardangrigoryan1 <111245763+vardangrigoryan1@users.noreply.github.com>
2026-08-02 10:43:19 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from ursina import *
 from ursina.prefabs.editor_camera import EditorCamera
 
-app = Ursina()
-window.title = "Wall-AI"
+#app = Ursina()
+#window.title = "Wall-AI"
+app = Ursina(title="Wall-AI - V.G.") #AFTER SENT
 window.cog_button.enabled = False
+window.entity_counter.enabled = False #AFTER SENT
+window.collider_counter.enabled = False #AFTER SENT
 EditorCamera()
 
 import environmental_mechanics
