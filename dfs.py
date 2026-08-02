97eb48323b91fbff37bb301582f74206f678c692
vardangrigoryan1 <111245763+vardangrigoryan1@users.noreply.github.com>
2026-08-02 10:38:16 +0400

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -13,7 +13,7 @@ def _dfs(grid, start, goal, hazards_positions):
             return _reconstruct(came_from, current)
 
         for next_x, next_z, _cost in _neighbors_of(grid, *current, hazards_positions):
-            next_cell = (next_x, next_x)
+            next_cell = (next_x, next_z) #AFTER SENT
             if next_cell not in visited:
                 visited.add(next_cell)
                 came_from[next_cell] = current
